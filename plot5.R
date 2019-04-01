935c9947ae8fcbd07c7b04f614628a60d7a0df13
Anil Maharjan <anil_pas@hotmail.com>
2019-04-01 16:52:19 +0545

    Add files via upload

---
+++ plot5.R
@@ -0,0 +1,57 @@
+# Load ggplot2 library
+
+library(ggplot2)
+
+
+
+# Loading provided datasets - loading from local machine
+
+
+NEI <- readRDS("C:/Users/amaharjan/Desktop/Coursera/exploratory data analysis/summarySCC_PM25.rds")
+
+SCC <- readRDS("C:/Users/amaharjan/Desktop/Coursera/exploratory data analysis/Source_Classification_Code.rds")
+
+
+NEI$year <- factor(NEI$year, levels=c('1999', '2002', '2005', '2008'))
+
+
+
+
+
+# How have emissions from motor vehicle sources changed from 1999-2008 in Baltimore City?
+
+
+
+# 24510 is Baltimore, see plot2.R
+
+# Searching for ON-ROAD type in NEI
+
+# Don't actually know it this is the intention, but searching for 'motor' in SCC only gave a subset (non-cars)
+
+subsetNEI <- NEI[NEI$fips=="24510" & NEI$type=="ON-ROAD",  ]
+
+
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
+
+
+
+
+
+
+
+png("plot5.png", width=840, height=480)
+
+g <- ggplot(aggregatedTotalByYear, aes(factor(year), Emissions))
+
+g <- g + geom_bar(stat="identity") +
+        
+        xlab("year") +
+        
+        ylab(expression('Total PM'[2.5]*" Emissions")) +
+        
+        ggtitle('Total Emissions from motor vehicle (type = ON-ROAD) in Baltimore City, Maryland (fips = "24510") from 1999 to 2008')
+
+print(g)
+
+dev.off()
